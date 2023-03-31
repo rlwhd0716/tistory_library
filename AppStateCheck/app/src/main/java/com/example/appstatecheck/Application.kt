457cebf5b454fda6a0a632fde2289787708ad437
이기종 <rlwhd0716@gmail.com>
2023-03-31 10:47:38 +0900

    add lifecycle

--- AppStateCheck/app/src/main/java/com/example/appstatecheck/Application.kt
+++ AppStateCheck/app/src/main/java/com/example/appstatecheck/Application.kt
@@ -1,10 +1,33 @@
 package com.example.appstatecheck
 
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
 
 class Application: android.app.Application(), LifecycleObserver {
 
+    companion object {
+        var isBackground = true
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+//        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onForeground() {
+        Log.d(javaClass.name, "onForeground")
+        isBackground = false
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onBackground() {
+        Log.d(javaClass.name, "onBackground")
+        isBackground = true
+    }
+
 }
